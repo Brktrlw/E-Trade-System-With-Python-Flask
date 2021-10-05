43ae6897a92e54e3130a9ca90e5094e4c3064403
BrktrL <74716514+Brktrlw@users.noreply.github.com>
2021-10-05 21:11:05 +0300

    Update

--- Python WebSite/deneme.py
+++ Python WebSite/deneme.py
@@ -1,16 +1,36 @@
 
 
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
+from flask_sqlalchemy import SQLAlchemy
 
-from datetime import datetime
-year = datetime.now().year
-month = datetime.now().month
-day = datetime.now().day
-hour =datetime.now().hour
-minute=datetime.now().minute
+app=Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///market.db"
+db=SQLAlchemy(app)
+class Product(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    name=db.Column(db.String(length=30),nullable=False)
+    price=db.Column(db.Integer(),nullable=False)
 
 
-date=str(day)+"/"+str(month)+"/"+str(year)+"-"+str(hour)+":"+str(minute)
-print(date)
+@app.route("/")
+def homePage():
+    return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+#from datetime import datetime
+#year = datetime.now().year
+#month = datetime.now().month
+#day = datetime.now().day
+#hour =datetime.now().hour
+#minute=datetime.now().minute
+
+
+#date=str(day)+"/"+str(month)+"/"+str(year)+"-"+str(hour)+":"+str(minute)
+#print(date)
+
 
 
 
